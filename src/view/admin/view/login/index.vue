f39be7fa3bb0bcd8b4ea4043d60b968b4112adbe
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2024-02-05 12:12:45 +0700

    fix login admin

--- src/view/admin/view/login/index.vue
+++ src/view/admin/view/login/index.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="w-full p-10 absolute inset-0 flex flex-col justify-center items-center gap-5">
+  <div class="w-full p-5 absolute inset-0 flex flex-col justify-center items-center gap-3">
     <h1 class="text-center text-xl">Masuk Sebagai Admin</h1>
+    <span class="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1" v-if="!isLoggin"> Gagal Login Sebagai Admin, <br />Cek Kembali Username dan password Anda </span>
     <form @submit.prevent="login" class="max-w-sm mx-auto">
       <div class="mb-5">
         <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">username</label>
@@ -15,12 +16,25 @@
       <div class="mb-5">
         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">password</label>
         <input
-          type="password"
+          :type="showPass ? 'text' : 'password'"
           id="password"
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           v-model="user.password"
           required
         />
+      </div>
+      <div class="flex items-start mb-5">
+        <div class="flex items-center h-5">
+          <input
+            id="show_pass"
+            type="checkbox"
+            value=""
+            v-model="showPass"
+            class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+            required
+          />
+        </div>
+        <label for="show_pass" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Show Password</label>
       </div>
       <button
         type="submit"
@@ -41,10 +55,13 @@ export default {
         username: "",
         password: "",
       },
+      isLoggin: true,
+      showPass: false,
     };
   },
   methods: {
     async login() {
+      this.isLoggin = true;
       axiosInstance
         .post("/api/admin/login", this.user)
         .then((result) => {
@@ -54,6 +71,11 @@ export default {
         })
         .catch((err) => {
           console.error(err);
+          this.isLoggin = false;
+          this.user = {
+            username: "",
+            password: "",
+          };
         });
     },
   },
