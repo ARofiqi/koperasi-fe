bfb92b05b1a82fcd3c8f1e0a00c5864375a514d5
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2024-02-05 11:53:39 +0700

    add design in login form admin

--- src/view/admin/view/login/index.vue
+++ src/view/admin/view/login/index.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="bg-green-500 w-full p-10 absolute inset-0 flex flex-col justify-center items-center gap-5">
+  <div class="w-full p-10 absolute inset-0 flex flex-col justify-center items-center gap-5">
     <h1 class="text-center text-xl">Masuk Sebagai Admin</h1>
-    <form @submit.prevent="login" class="w-1/3">
-      <div class="grid grid-cols-2 p-3">
-        <label for="username"> username </label>
-        <input type="text" v-model="user.username" required />
+    <form @submit.prevent="login" class="max-w-sm mx-auto">
+      <div class="mb-5">
+        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">username</label>
+        <input
+          type="text"
+          id="username"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          v-model="user.username"
+          required
+        />
       </div>
-      <div class="grid grid-cols-2 p-3">
-        <label for="password"> password </label>
-        <input type="password" v-model="user.password" required />
+      <div class="mb-5">
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">password</label>
+        <input
+          type="password"
+          id="password"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          v-model="user.password"
+          required
+        />
       </div>
-      <button type="submit" class="m-3 py-1 px-3 bg-blue-600 rounded-lg text-white">Login</button>
+      <button
+        type="submit"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
     </form>
   </div>
 </template>
@@ -33,7 +50,6 @@ export default {
         .then((result) => {
           const token = result.token;
           localStorage.setItem("token", token);
-
           this.$router.push("/dashboard");
         })
         .catch((err) => {
